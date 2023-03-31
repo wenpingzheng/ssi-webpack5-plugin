21df3d7f2e059ddf26d41e45bf2143f30724e942
wpzheng <wpzheng@tencent.com>
2023-03-31 10:54:25 +0800

    feat: remove console

--- utils/path-rewrite.js
+++ utils/path-rewrite.js
@@ -1,8 +1,8 @@
-var _ = require('lodash')
+var _ = require('lodash');
 
 module.exports = {
-  create: createPathRewriter
-}
+  create: createPathRewriter,
+};
 
 /**
  * Create rewrite function, to cache parsed rewrite rules.
@@ -10,61 +10,61 @@ module.exports = {
  * @param {Object} rewriteConfig
  * @return {Function} Function to rewrite paths; This function should accept `path` (request.url) as parameter
  */
-function createPathRewriter (rewriteConfig) {
-  var rulesCache
+function createPathRewriter(rewriteConfig) {
+  var rulesCache;
   if (!isValidRewriteConfig(rewriteConfig)) {
-    return
+    return;
   }
 
   if (_.isFunction(rewriteConfig)) {
-    var customRewriteFn = rewriteConfig
-    return customRewriteFn
+    var customRewriteFn = rewriteConfig;
+    return customRewriteFn;
   } else {
-    rulesCache = parsePathRewriteRules(rewriteConfig)
-    return rewritePath
+    rulesCache = parsePathRewriteRules(rewriteConfig);
+    return rewritePath;
   }
 
-  function rewritePath (path) {
-    var result = path
+  function rewritePath(path) {
+    var result = path;
 
     _.forEach(rulesCache, function (rule) {
       if (rule.regex.test(path)) {
-        result = result.replace(rule.regex, rule.value)
-        return false
+        result = result.replace(rule.regex, rule.value);
+        return false;
       }
-    })
+    });
 
-    return result
+    return result;
   }
 }
 
-function isValidRewriteConfig (rewriteConfig) {
+function isValidRewriteConfig(rewriteConfig) {
   if (_.isFunction(rewriteConfig)) {
-    return true
+    return true;
   } else if (!_.isEmpty(rewriteConfig) && _.isPlainObject(rewriteConfig)) {
-    return true
-  } else if (_.isUndefined(rewriteConfig) ||
-               _.isNull(rewriteConfig) ||
-               _.isEqual(rewriteConfig, {})) {
-    return false
+    return true;
+  } else if (
+    _.isUndefined(rewriteConfig) ||
+    _.isNull(rewriteConfig) ||
+    _.isEqual(rewriteConfig, {})
+  ) {
+    return false;
   } else {
-    throw new Error("path rewrite error");
+    throw new Error('path rewrite error');
   }
 }
 
-function parsePathRewriteRules (rewriteConfig) {
-  var rules = []
+function parsePathRewriteRules(rewriteConfig) {
+  var rules = [];
 
   if (_.isPlainObject(rewriteConfig)) {
     _.forIn(rewriteConfig, function (value, key) {
       rules.push({
         regex: new RegExp(key),
-        value: rewriteConfig[key]
-      })
-    })
+        value: rewriteConfig[key],
+      });
+    });
   }
 
-  return rules
+  return rules;
 }
-
-
